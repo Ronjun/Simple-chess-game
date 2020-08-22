b3d48afdcaccee71ee27e1f90e0e5824bf288658
Ronaldo Junior <ronaldocosta2603@gmail.com>
2020-08-22 12:27:59 -0300

    Implementing checkmate

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -16,6 +16,7 @@ public class ChessMatch {
 	private int turn;
 	private Color currentPlayer;
 	private boolean check;
+	private boolean checkmate;
 	
 	private List<Piece> piecesOnTheBoard = new ArrayList<>();
 	private List<Piece> capturedPieces = new ArrayList<>();
@@ -46,6 +47,9 @@ public class ChessMatch {
 		return mat;
 	}
 	
+	public boolean getCheckmate() {
+		return checkmate;
+	}
 	public boolean[][] possibleMoves(ChessPosition sourcePosition){
 		Position position = sourcePosition.toPosition();
 		validateSourcePosition(position);
@@ -64,8 +68,12 @@ public class ChessMatch {
 		}
 		
 		check = (testCheck(opponent(currentPlayer)))? true:false;
-		nextTurn();
-		
+		if(testCheckmate(opponent(currentPlayer))) {
+			checkmate = true;
+		}
+		else {
+			nextTurn();
+		}
 		return (ChessPiece)capturedPiece;
 	}
 	
@@ -142,6 +150,30 @@ public class ChessMatch {
 		return false;
 	}
 	
+	private boolean testCheckmate(Color color) {
+		if(!testCheck(color)) {
+			return false;
+		}
+		List<Piece> list = piecesOnTheBoard.stream().filter(x -> ((ChessPiece)x).getColor() == color).collect(Collectors.toList());
+		for(Piece p : list) {
+			boolean[][] mat = p.possibleMoves();
+			for(int i= 0; i<board.getRows(); i++) {
+				for(int j=0;j<board.getColumns();j++) {
+					if(mat[i][j]) {
+						Position source = ((ChessPiece)p).getChessPosition().toPosition();
+						Position target = new Position(i,j);
+						Piece capturedPiece = makeMove(source, target);
+						boolean testCheck = testCheck(color);
+						undoMove(source, target, capturedPiece);
+						if(!testCheck) {
+							return false;
+						}
+					}
+				}
+			}
+		}
+		return true;
+	}
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
 		piecesOnTheBoard.add(piece);
